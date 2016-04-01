53354223370ed5225cb9dfdde3d93a1b3c66dfb6
chantebryana <chantebryana@gmail.com>
2016-04-01 15:22:26 -0500

    Nil + Load File
    Add "nil" to end of If...Else (to remove Return value); reminder how to
    change directories and load file into Ruby Command Prompt.  It's the best!

--- RubyHangmanTesting02.rb
+++ RubyHangmanTesting02.rb
@@ -1,23 +1,23 @@
 #Successful discoveries
 word = "Quash"
 letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
-underscore = letter.map { |x| x = "_" } # => ["_", "_", "_", "_", "_"]
-number = word.length # => 5
-number.times { print "_ " } # => "_ _ _ _ _ "
-underscore.join(" ") # => "_ _ _ _ _"
-spaces = underscore.join(" ") # => "_ _ _ _ _"
-print spaces # => "_ _ _ _ _"
+#underscore = letter.map { |x| x = "_" } # => ["_", "_", "_", "_", "_"]
+#number = word.length # => 5
+#number.times { print "_ " } # => "_ _ _ _ _ "
+#spaces = underscore.join(" ") # => "_ _ _ _ _"
+#print spaces # => "_ _ _ _ _"
 
 
-guess = "l"
+guess = "s"
 letter.each do |i|
   if guess == i 
     print guess.downcase + " "
   else 
     print "_ "
   end
-end
+end;nil # => adding ";nil" after final end will change the return statement from "letter" array to "nil"
 
+#How to load my file into CMD instead of manually typing each time!!
 =begin
 irb(main):051:0> letter.each do |i|
 irb(main):052:1*   if guess == i
@@ -29,3 +29,4 @@ irb(main):057:1> end
 _ _ _ s _ => ["q", "u", "a", "s", "h"]
 irb(main):058:0>
 =end
+
