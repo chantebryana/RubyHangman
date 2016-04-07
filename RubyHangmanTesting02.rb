cbb891800d0dfe7edf6fb8a496478b5384316b83
chantebryana <chantebryana@gmail.com>
2016-04-07 15:07:52 -0500

    Redefine Display: gets to array
    Instead of trying to save the results of P2's guesses into ongoingly-saved
    variable arrays, I decide to run a loop each time P2's ready to guess.
    P2's guesses get added to a gets array, and that array adds over time.
    Then I run the changing array to a static loop and static variables, which
    display P2's ongoing guesses as the game progresses.  I still have a long
    ways to go, but these two files represent the initial fledgling of changes
    in that vein.

--- RubyHangmanTesting02.rb
+++ RubyHangmanTesting02.rb
@@ -1,32 +1,41 @@
 #Successful discoveries
-word = "Quash"
+#graveyardArray = []
+#correctArray = []
+word = "Squish"
 letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
 #underscore = letter.map { |x| x = "_" } # => ["_", "_", "_", "_", "_"]
 #number = word.length # => 5
 #number.times { print "_ " } # => "_ _ _ _ _ "
 #spaces = underscore.join(" ") # => "_ _ _ _ _"
+# alternative: ["_", "_", "_"] * " " => "_ _ _"
 #print spaces # => "_ _ _ _ _"
 
+guess = "s" 
+#gets => concatinated at end of array 'guess'
+guess = ["s", "e", "f", "q"]
+print "WORD: "
 
-guess = "s"
 letter.each do |i|
-  if guess == i 
-    print guess.downcase + " "
-  else 
-    print "_ "
+  guess.each do |j|
+    if i == j 
+      print j.downcase + " "
+	  # add to "correct" array
+	  # if "correct" array is full (if there are no underscores remaining), then P2 wins
+    else 
+      print "_ "
+	  # add to "incorrect" array
+	  # if graveyard array is full (== 6 elements), then P2 loses
+    end
+    # add P2 guess to "guessed-array"
   end
-end;nil # => adding ";nil" after final end will change the return statement from "letter" array to "nil"
+end 
 
-#How to load my file into CMD instead of manually typing each time!!
 =begin
-irb(main):051:0> letter.each do |i|
-irb(main):052:1*   if guess == i
-irb(main):053:2>     print guess.downcase + " "
-irb(main):054:2>   else
-irb(main):055:2*     print "_ "
-irb(main):056:2>   end
-irb(main):057:1> end
-_ _ _ s _ => ["q", "u", "a", "s", "h"]
-irb(main):058:0>
-=end
+print "GRAVEYARD: "
 
+letter.each do |i|
+  if guess != i 
+    print guess + ", "
+  end
+end
+=end
---
+++ scratchPad.rb
@@ -0,0 +1,5 @@
+testArray = ["a", "b", "c", "d", "e", "f"]
+arrayLength = testArray.length
+Array.new(arrayLength) #=> [nil, nil, nil, nil, nil, nil]
+testLetter = "d"
+
