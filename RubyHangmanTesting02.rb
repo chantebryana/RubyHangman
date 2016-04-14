99f0db436e6fc42a6bbd678ffcdbc9c14bfee4e7
chantebryana <chantebryana@gmail.com>
2016-04-14 17:05:38 -0500

    Partial Success: Graveyard Portion
    Succeed at properly debugging and correcting problem to get partial
    victory on Graveyard printout.  Line 41 (if letterx == guessx) is the
    crutial line.  The order of Letter and Guess matters a lot for what should
    be printed.  The operand is also very important: I struggled with != and
    (by mistake) a single =.  It was only after I changed all of the variables
    of this Graveyard loop to match "exactly" with the Word loop that I found
    my error: wrong operand meant wrong printout.  I still have further to go
    to get this Graveyard portion the way I want, but I'm proud that I
    successfully debugged my own problem!  It's crazy how a single character
    can confuse the hell out of you!

--- RubyHangmanTesting02.rb
+++ RubyHangmanTesting02.rb
@@ -3,6 +3,7 @@
 #correctArray = []
 word = "squish"
 letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
+guessArray = []
 #underscore = letter.map { |x| x = "_" } # => ["_", "_", "_", "_", "_"]
 #number = word.length # => 5
 #number.times { print "_ " } # => "_ _ _ _ _ "
@@ -10,22 +11,18 @@ letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
 # alternative: ["_", "_", "_"] * " " => "_ _ _"
 #print spaces # => "_ _ _ _ _"
 
-#P2: make a guess: (P2 inputs single layer, presses ENTER)
-#guess = gets.upcase
-#guessArray = []
-#guessArray << guess ; => ["s", "e", "f", "q"], etc
-guessArray = ["s", "e", "f", "q"]
+#print "P2, guess a letter: " #(P2 inputs single layer, presses ENTER)
+#guess = gets.chomp.downcase
+#guessArray << guess #=> ex: ["s", "e", "f", "q"]
+guessArray = ["u", "e", "f", "q"]
 print "WORD: "
 
 letter.map do |letterz|
   have_we_found_this_letter_in_the_guessed_list_yet = false
   guessArray.map do |guessz|
     if letterz == guessz
-      #print i.downcase + " "
 	  have_we_found_this_letter_in_the_guessed_list_yet = true
 	  break
-    #else 
-      #print "_ "
     end
   end
   if have_we_found_this_letter_in_the_guessed_list_yet
@@ -35,12 +32,21 @@ letter.map do |letterz|
   end
 end 
 
-=begin
+
 print "GRAVEYARD: "
 
-letter.each do |i|
-  if guess != i 
-    print guess + ", "
-  end
-end
-=end
+guessArray.map do |guessx|
+  thisLetterDoesntMatchTheWord = true
+  letter.map do |letterx|
+	  if letterx == guessx #this order matters, apparently; also the equals operand is crutial!!
+		thisLetterDoesntMatchTheWord = false
+		break
+	  end
+	end
+	  if thisLetterDoesntMatchTheWord
+		print guessx + " "
+	  else
+		print "_ "
+	  end
+	end
+
