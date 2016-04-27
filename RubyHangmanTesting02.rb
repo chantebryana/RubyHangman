65101359a799baae5e3e4e3d3baa28f33e0d618c
chantebryana <chantebryana@gmail.com>
2016-04-27 11:23:52 -0500

    Attempt at looping the whole game for playing
    The loop is infinite (CTRL + C to exit) but each iteration loses
    information from previous passthru.  Figure out how to remember old
    variables!

--- RubyHangmanTesting02.rb
+++ RubyHangmanTesting02.rb
@@ -1,40 +1,41 @@
-word = "squish"
-letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
-guessArray = []
-#print "P2, guess a letter: " #(P2 inputs single layer, presses ENTER)
-#guess = gets.chomp.downcase
-#guessArray << guess #=> ex: ["s", "e", "f", "q"]
-#loop: while true; add break condition later on
-guessArray = ["u", "e", "f", "q"]
-print "WORD: "
-letter.map do |letterz|
-  have_we_found_this_letter_in_the_guessed_list_yet = false
-  guessArray.map do |guessz|
-    if letterz == guessz
-	  have_we_found_this_letter_in_the_guessed_list_yet = true
-	  break
-    end
-  end
-  if have_we_found_this_letter_in_the_guessed_list_yet
-    print letterz + " "
-  else
-    print "_ "
-  end
+loop do 
+	word = "squish"
+	letter = word.downcase.scan(/./) # => ["q", "u", "a", "s", "h"]
+	guessArray = []
+	print "P2, guess a letter: " #(P2 inputs single layer, presses ENTER)
+	guess = gets.chomp.downcase
+	guessArray << guess #=> ex: ["s", "e", "f", "q"]
+	#loop: while true; add break condition later on
+	#guessArray = ["u", "e", "f", "q"]
+	print "WORD: "
+	letter.map do |letterz|
+	  have_we_found_this_letter_in_the_guessed_list_yet = false
+	  guessArray.map do |guessz|
+		if letterz == guessz
+		  have_we_found_this_letter_in_the_guessed_list_yet = true
+		  break
+		end
+	  end
+	  if have_we_found_this_letter_in_the_guessed_list_yet
+		print letterz + " "
+	  else
+		print "_ "
+	  end
+	end 
+	print "GRAVEYARD: "
+	graveyardTally = 6
+	guessArray.map do |guessx|
+	  thisLetterDoesntMatchTheWord = true
+	  letter.map do |letterx|
+		  if letterx == guessx 
+			thisLetterDoesntMatchTheWord = false
+			break
+		  end
+		end
+		  if thisLetterDoesntMatchTheWord
+			print guessx + " "
+			graveyardTally -= 1
+		  end
+		end
+	print "| " + graveyardTally.to_s + " wrong guesses remain \n"
 end 
-print "GRAVEYARD: "
-#graveyardArray = Array.new(6, "")
-graveyardTally = 6
-guessArray.map do |guessx|
-  thisLetterDoesntMatchTheWord = true
-  letter.map do |letterx|
-	  if letterx == guessx # because of my double-breaks, this is effectively letter.0 == guessx (and on and on)
-		thisLetterDoesntMatchTheWord = false
-		break
-	  end
-	end
-	  if thisLetterDoesntMatchTheWord
-		print guessx + " "
-		graveyardTally -= 1
-	  end
-	end
-print "| " + graveyardTally.to_s + " wrong guesses remain"
