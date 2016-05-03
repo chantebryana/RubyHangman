199b4de34df3223437539764786be9f508fe2536
chantebryana <chantebryana@gmail.com>
2016-05-02 22:09:45 -0500

    Cleaning up my mess
    Jim fucked everything up.

--- RubyHangmanTesting02.rb
+++ RubyHangmanTesting02.rb
@@ -1,34 +1,26 @@
 #P1 enters the hangman word:
-print "P1, enter a word: " #(P1 inputs whole word, presses ENTER)
-#word = "squish"
-#letter = word.downcase.scan(/./) 
+print "P1, enter a single real English word, then press ENTER: " #(P1 inputs whole word, presses ENTER)
 word = gets.chomp.downcase 
 letter = word.scan(/./) # => ["q", "u", "a", "s", "h"]
 letterTally = letter.length #a potential marker for winning condition?
 guessArray = []
-#graveyardTally = 6
 
-#=begin
 #create enough new lines to separate P1's word from P2's guesses:
 x = 0
 while x <= 60
   puts "###"
   x += 1
 end
-#=end
 
 #initiate P2's guessing loop:
 loop do
 	print "P2, guess a letter: " #(P2 inputs single layer, presses ENTER)
 	guess = gets.chomp.downcase
 	guessArray << guess #=> ex: ["s", "e", "f", "q"]
-	#loop: while true; add break condition later on
-	#guessArray = ["u", "e", "f", "q"]
 	print "WORD: "
 	guessTally = 0
 	letter.map do |letterz|
 	  have_we_found_this_letter_in_the_guessed_list_yet = false
-	  #$guessTally = 0
 	  guessArray.map do |guessz|
 		if letterz == guessz
 		  have_we_found_this_letter_in_the_guessed_list_yet = true
@@ -42,7 +34,6 @@ loop do
 		print "_ "
 	  end
 	end 
-	print guessTally.to_s + "\n"
 	print "GRAVEYARD: "
 	graveyardTally = 6
 	guessArray.map do |guessx|
